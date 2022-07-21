309b08d5b0aaafa681344bce83bcaac7034cb875
João de Jesus Costa <joao.j.costa@criticalsoftware.com>
2022-07-21 21:36:10 +0100

    Add: SessionResource tests native version.

---
+++ backend/CriticalCards/src/test/java/com/criticalsoftware/SessionResourceIT.java
@@ -0,0 +1,9 @@
+package com.criticalsoftware;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class SessionResourceIT extends SessionResourceTest {
+
+    // Execute the same tests but in native mode.
+}

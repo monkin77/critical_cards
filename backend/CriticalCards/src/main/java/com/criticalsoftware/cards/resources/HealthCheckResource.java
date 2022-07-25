3e46ec6497b207ddb6776f98aa5b86cbbd4674aa
João Mesquita <cswsis22-jme@criticalsoftware.com>
2022-07-25 12:19:26 +0100

    Add: metrics endpoint

--- backend/CriticalCards/src/main/java/com/criticalsoftware/cards/resources/HealthCheckResource.java
+++ backend/CriticalCards/src/main/java/com/criticalsoftware/cards/resources/HealthCheckResource.java
@@ -4,11 +4,14 @@ import com.criticalsoftware.cards.entities.Cards_Session;
 import com.criticalsoftware.cards.entities.Retro_Card;
 import com.criticalsoftware.cards.entities.Retro_Lane;
 
+import javax.json.Json;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("health")
 public class HealthCheckResource {
@@ -42,4 +45,30 @@ public class HealthCheckResource {
         }
     }
 
+    @GET
+    @Path("metrics")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response metrics() {
+        final List<Cards_Session> sessions = Cards_Session.streamAll().limit(10).map(cs -> (Cards_Session) cs).collect(Collectors.toList());
+        int numSessions = sessions.size();
+
+        if (numSessions == 0)
+            return Response.status(204).build();
+
+        long start = System.currentTimeMillis();
+        for (Cards_Session session : sessions) {
+            Response res = new RetroResource().getRetro(session.id);
+            if (res.getStatusInfo().getStatusCode() != 200)
+                return Response.status(500).entity("Error fetching Retro Resource with id: " + session.id + ".").build();
+            System.out.println("Session: " + session.id);
+        }
+        long end = System.currentTimeMillis();
+
+        long meanTime = (end - start) / numSessions;
+        String body = Json.createObjectBuilder()
+                .add("duration", meanTime)
+                .add("numSamples", numSessions)
+                .build().toString();
+        return Response.ok(body).build();
+    }
 }

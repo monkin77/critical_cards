31d3ec79331423f90086845421c25487c29a27c4
DuBento <duarte.bento@tecnico.ulisboa.pt>
2022-07-25 11:02:38 +0100

    Header buttons and flex display

--- frontend/src/app/retro/retro.component.ts
+++ frontend/src/app/retro/retro.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import { ActivatedRoute, Router } from '@angular/router';
+import { RetroSession } from '../DTOs/retro-session';
 
 @Component({
   selector: 'app-retro',
   templateUrl: './retro.component.html',
-  styleUrls: ['./retro.component.scss']
+  styleUrls: ['./retro.component.scss'],
 })
 export class RetroComponent implements OnInit {
-  sessionId:String|null;
+  sessionId: String | null;
+  retroSession: RetroSession | null = null;
 
-  constructor(route: ActivatedRoute, router:Router) {
-    this.sessionId = route.snapshot.paramMap.get("id");
-    if(this.sessionId==null)
-      router.navigate(['pageNotFound']);
-
+  constructor(route: ActivatedRoute, router: Router) {
+    this.sessionId = route.snapshot.paramMap.get('id');
+    if (this.sessionId == null) router.navigate(['pageNotFound']);
+    else {
+      // TODO get retro from service
+      this.retroSession = { id: -1, name: 'Mock Retro', lanes: [] };
+    }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   editRetroName() {
-    console.log("TODO, Should edit");
+    console.log('TODO, Should edit');
   }
 
+  back() {
+    console.log('TODO, Should navigate back');
+  }
+
+  addLane() {
+    console.log('TODO, Should add lane');
+  }
 }
